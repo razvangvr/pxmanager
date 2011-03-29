384cab80c442f6bdad75a4e8a76f0cf8b95d38c7
razvan.gaston@gmail.com <razvan.gaston@gmail.com@88331ca1-f94c-c32a-6f21-ca0ba22567e4>
2011-03-29 13:29:46 +0000

    Work on ExpresionEvalTask

---
+++ trunk/ProgramingTaskExpresionEval/src/raz/test/expresion/util/ExpresionManipulator.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package raz.test.expresion.util;
+
+/**
+ *
+ * @author razvang
+ */
+public class ExpresionManipulator {
+
+    /**
+     * This method removes the <b>eval</b> key-word from the string
+     * @return returns the String without eval
+     */
+    public static String removeEvalKeyWord(String expresion) {
+        String result = null;
+        result = expresion.substring(Assert.EVAL.length(), expresion.length());
+        return result;
+    }
+
+    /**
+     * This method removes the round Parenthesis from the string
+     * @return the String without ()
+     */
+    public static String removeParenthesis(String expresion) {
+        String result = null;
+        int beginIndex = expresion.indexOf(Assert.ROUND_PARENTHESIS_BEGIN);
+        if (!(beginIndex == 0)) {
+            //Pozitia pe care ar trebui sa fie ( ar trebui sa fie 0
+            throw new IllegalStateException("( - was not found at the begining of the Expresion");
+        }
+        int endIndex = expresion.indexOf(Assert.ROUND_PARENTHESIS_END);
+        if (!(endIndex == expresion.length() - 1)) {
+            //Pozitia pe care ar trebui sa fie ) ar trebui sa fie expresion.length - 1
+            throw new IllegalStateException(") - was not found at the end of the Expresion");
+        }
+        result = expresion.substring(beginIndex+1, endIndex);
+        return result;
+    }
+}
