51f64f3af7f53e94fa3d5bce01bf669f8305367e
razvan.gaston@gmail.com <razvan.gaston@gmail.com@88331ca1-f94c-c32a-6f21-ca0ba22567e4>
2011-03-31 14:13:30 +0000

    fix unit tests

---
+++ trunk/ProgramingTaskExpresionEval/src/raz/test/command/Command.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package raz.test.command;
+
+/**
+ *
+ * @author razvang
+ */
+public interface Command {
+
+    public void execute();
+
+}
