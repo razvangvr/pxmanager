e9e173257a4f72d49ba919ca38057b50836c43b0
razvan.gaston@gmail.com <razvan.gaston@gmail.com@88331ca1-f94c-c32a-6f21-ca0ba22567e4>
2012-03-29 15:06:23 +0000

    org.eclipse.views Tutorial

---
+++ branches/eclipseProjects/org.eclipse.ui.articles.views/src/org/eclipse/ui/articles/views/Perspective.java
@@ -0,0 +1,17 @@
+package org.eclipse.ui.articles.views;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		/*
+		 * layout.addView() adds a view to the perspective. You can also add
+		 * placeholders for views via the layout.addPlaceholder() method call.
+		 */
+//		layout.addView("org.eclipse.ui.articles.views.labelview",
+//				IPageLayout.TOP, IPageLayout.RATIO_MAX,
+//				IPageLayout.ID_EDITOR_AREA);
+	}
+}
