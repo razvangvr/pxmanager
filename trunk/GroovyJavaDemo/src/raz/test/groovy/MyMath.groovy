0700a5f298d9ba4d29f45b15bc7585add8e8152b
razvan.gaston@gmail.com <razvan.gaston@gmail.com@88331ca1-f94c-c32a-6f21-ca0ba22567e4>
2012-06-27 06:13:42 +0000

    groovy demo

---
+++ trunk/GroovyJavaDemo/src/raz/test/groovy/MyMath.groovy
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package raz.test.groovy
+
+/**
+ *
+ * @author rgaston
+ */
+class MyMath {
+    
+    public static int square(int param) {
+        return param*param;
+    }
+	
+}
+
