60e8fe96fd67c1c7f83ad0c2f9208bd9a9215f3b
razvan.gaston@gmail.com <razvan.gaston@gmail.com@88331ca1-f94c-c32a-6f21-ca0ba22567e4>
2013-02-19 15:07:19 +0000

    tutorial si exercitii Quartz

---
+++ razQuartz/src/main/java/raz/test/quartz/App.java
@@ -0,0 +1,13 @@
+package raz.test.quartz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
