c7bd1d648cf33d261cc02dff5596c6c0e3cac390
razvan.gaston@gmail.com <razvan.gaston@gmail.com@88331ca1-f94c-c32a-6f21-ca0ba22567e4>
2013-02-20 14:37:13 +0000

    quartz tutorial

---
+++ razQuartz/src/main/java/raz/test/quartz/RazExampleJob.java
@@ -0,0 +1,41 @@
+package raz.test.quartz;
+
+import java.util.Date;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RazExampleJob implements Job{
+	
+	private static Logger logger = LoggerFactory.getLogger(RazExampleJob.class);
+	
+	//Quartz requires public constructor
+	public RazExampleJob(){}
+
+	/**
+     * <p>
+     * Called by the <code>{@link org.quartz.Scheduler}</code> when a
+     * <code>{@link org.quartz.Trigger}</code> fires that is associated with
+     * the <code>Job</code>.
+     * </p>
+     * 
+     * @throws JobExecutionException
+     *             if there is an exception while executing the job.
+     */
+	public void execute(JobExecutionContext context)
+			throws JobExecutionException {
+		
+		// This job simply prints out its job name and the
+        // date and time that it is running
+        JobKey jobKey = context.getJobDetail().getKey();
+        logger.info(">> " + jobKey + " executing at " + new Date());
+		//System.out.println(">> " + jobKey + " executing at " + new Date());
+		
+	}
+
+}
