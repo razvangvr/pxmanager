2249490ed42bb65503658ae288df44a405af0ed7
razvan.gaston@gmail.com <razvan.gaston@gmail.com@88331ca1-f94c-c32a-6f21-ca0ba22567e4>
2013-02-23 16:50:36 +0000

    callable examples

---
+++ raz.test.threads/src/raz/callable/StringComputation.java
@@ -0,0 +1,49 @@
+package raz.callable;
+
+/**
+ * this is a computation that takes long time to complete
+ * it return the length of a String
+ * */
+class StringComputation implements Runnable{
+	private String word;
+	private Integer result = null;
+	Thread  t;
+	
+	public Integer getResult(){
+		return result;
+	}
+	
+	/**
+	 * returns a reference to the ThreadObject which is performing this taks
+	 * */
+	public Thread getThread(){
+		return t;
+	}
+	
+	public StringComputation(String word){
+		this.word = word;
+		t = new Thread(this);
+		t.start();
+	}
+	
+	/**
+	 * this method takes a lot of time to process
+	 * */
+	private void compute(){
+		result =  Integer.valueOf(word.length());
+		//To simulate this, we use thread.sleep
+		try {
+			Thread.currentThread().sleep(5*RazLongCalculationSubThread.SECOND);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		RazLongCalculationSubThread.debugThreadInfo("Done, result:"+result);
+	}
+
+	//do the long running task on another thread
+	@Override
+	public void run() {
+		compute();
+		
+	}
+}
