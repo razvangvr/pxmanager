55c59335c068846f00429abf4206d83c7f18ea73
razvan.gaston@gmail.com <razvan.gaston@gmail.com@88331ca1-f94c-c32a-6f21-ca0ba22567e4>
2013-03-24 21:13:39 +0000

    First Spring Tutorial. Dependency Injection - annotations

---
+++ spring.di.annotation.first/src/main/java/annotations/testbean/MySpringBeanWithDependency.java
@@ -0,0 +1,24 @@
+package annotations.testbean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import annotations.writer.IWriter;
+
+
+@Service
+public class MySpringBeanWithDependency {
+	
+	private IWriter writer;
+	
+	@Autowired
+	public void setWriter(IWriter writer){
+		this.writer = writer;
+	}
+	
+	public void run(){
+		String s = "This is my test";
+		writer.write(s);
+	}
+
+}
